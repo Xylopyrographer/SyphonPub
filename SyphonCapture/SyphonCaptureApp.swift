b89606d1ca1de8c286cad4bae61c13773694ca07
Xylopyrographer <xylopyrographer@gmail.com>
2026-04-14 15:51:27 -0600

    Initial Commit

---
+++ SyphonCapture/SyphonCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyphonCaptureApp.swift
+//  SyphonCapture
+//
+//  Created by Rob on 2026-04-14.
+//
+
+import SwiftUI
+
+@main
+struct SyphonCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
